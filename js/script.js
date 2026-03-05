9dba1e63b6189a1c62752003d3ca6da754e19e23
Joao pedro vieira <joaovieirapedro1@gmail.com>
2026-03-05 17:26:25 -0300

    retirada do zoom 2

--- js/script.js
+++ js/script.js
@@ -301,3 +301,17 @@ const sobre = document.getElementById("Sobre")
 sobre.addEventListener("click", () => {
     window.location.href = "../html/readme.html";
 });
+document.addEventListener('touchstart', function (event) {
+  if (event.touches.length > 1) {
+    event.preventDefault(); 
+  }
+}, { passive: false });
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  let now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
